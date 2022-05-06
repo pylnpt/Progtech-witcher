c76c3db5133329c94ee489834c37698b89c97892
BSX8GS <91091500+BSX8GS@users.noreply.github.com>
2022-05-06 19:21:26 +0200

    #feat(#8): Created and implemented User related classes
    Role, RoleBase, Employer, Employee, Admin, CanTakeJobs, CanAdvertiseJobs

---
+++ project/src/main/java/progtechbead/progtechapp/models/user/Admin.java
@@ -0,0 +1,24 @@
+package progtechbead.progtechapp.models.user;
+
+public class Admin extends UserBase{
+
+    public Admin(String username) {
+        super(username, Role.ADMIN);
+    }
+
+    public void DeleteUser(UserBase user)
+    {
+        if(user.getRole() != Role.ADMIN)
+        {
+            //delete user
+        }
+    }
+
+    public void ModifyRoleOfUser(UserBase user, Role role)
+    {
+        if(user.getRole()!=role)
+        {
+            user.setRole(role);
+        }
+    }
+}
